51b67aac2ec14c3d06f66044cdd1990b0b233d67
Jesse Stuart <hi@jessestuart.com>
2018-05-21 04:20:57 -0400

    [commit message skipped]

--- .circleci/build-image.sh
+++ .circleci/build-image.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-export IMAGE_ID="${IMAGE}:${VERSION}-${TAG}"
+git clone https://github.com/${GITHUB_REPO} --depth=1 &>/dev/null
+
 # ============
 # <qemu-support>
 if [ $GOARCH == 'amd64' ]; then
   touch qemu-amd64-static
 else
+
   curl -sL "https://github.com/multiarch/qemu-user-static/releases/download/${QEMU_VERSION}/qemu-${QEMU_ARCH}-static.tar.gz" | tar xz
   docker run --rm --privileged multiarch/qemu-user-static:register
 fi
@@ -14,6 +16,8 @@ fi
 
 # Replace the repo's Dockerfile with our own.
 cp -f $DIR/Dockerfile .
+
+export IMAGE_ID="${IMAGE}:${VERSION}-${TAG}"
 docker build -t ${IMAGE_ID} --build-arg target=$TARGET --build-arg arch=$QEMU_ARCH .
 
 # Login to Docker Hub.
--- .circleci/load_env.sh
+++ .circleci/load_env.sh
@@ -1,11 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
 echo 'export IMAGE=jessestuart/fluent-bit' >> $BASH_ENV
 echo 'export GITHUB_REPO=fluent/fluent-bit' >> $BASH_ENV
-curl -s https://api.github.com/repos/fluent/fluent-bit/releases/latest | \
-	grep tag_name | \
-	tr -d '"tag_name: ' | \
-	tr -d ',' > ./VERSION
+curl -s https://api.github.com/repos/fluent/fluent-bit/releases/latest | jq -r ".tag_name" > ./VERSION
 echo 'export VERSION=$(cat ./VERSION)' >> $BASH_ENV
 echo 'export IMAGE_ID="${REGISTRY}/${IMAGE}:${VERSION}-${TAG}"' >> $BASH_ENV
 echo 'export DIR=`pwd`' >> $BASH_ENV
